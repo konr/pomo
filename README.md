0793852ccce9c20c4055de30a09596fcefc986ef
Konrad Scorciapino <konr@konr.mobi>
2015-02-08 14:18:05 -0200

    sound

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # pomo
 Pomodoro timer
+
+# Todo
+
+* Better samples
+* Counting instead of tick
+* Log
+* data analysis
+
+# License
+
+Public domain
+
+# Sound effects
+
+* All sound effects from http://www.pdsounds.org/
+
--- pomo.py
+++ pomo.py
@@ -1,26 +1,47 @@
-from time import sleep
+import time
+import subprocess
 
-ONE_MINUTE = 1
+ONE_MINUTE = 60
+
+class Config:
+    def __init__(self, config):
+        self._config = config
+
+    def get(self, attr):
+        return self._config[attr]
+        
 
 class Timer:
-    def __init__(self, time):
-        self._time = time
+    def __init__(self, config):
+        self._config = config
 
     def __call__(self):
         self._timer_start()
-        for i in range(self._time):
-            sleep(ONE_MINUTE)
+        for i in range(self._config.get("minutes")):
+            time.sleep(ONE_MINUTE)
             self._minute_elapsed(i+1)
-        return self._timer_done()
+        return self._timer_end()
+
+    def _play(self, file):
+        subprocess.Popen([self._config.get("player"), file])
 
     def _minute_elapsed(self, i):
-        print "{}/{}".format(i, self._time)
+        self._play(self._config.get("minute_elapsed_sound"))
+        print "{}/{}".format(i, self._config.get("minutes"))
 
     def _timer_start(self):
+        self._play(self._config.get("timer_start_sound"))
         print "Starting."
 
     def _timer_end(self):
+        self._play(self._config.get("timer_end_sound"))
         print "Done!"
 
 if __name__ == '__main__':
-    Timer(25)()
+    config = Config({
+        "minutes": 25,
+        "minute_elapsed_sound": "resources/samples/tick.wav",
+        "timer_start_sound": "resources/samples/chiming pottery02.wav",
+        "timer_end_sound": "resources/samples/applause.wav",
+        "player": "paplay"})
+    Timer(config)()
